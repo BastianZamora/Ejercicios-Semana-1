d57226ef2a2dde6e7b247fc4408c012631dee6c4
Bastián Zamora <162739330+BastianZamora@users.noreply.github.com>
2024-03-12 17:33:07 -0300

    Update 2.py

--- Ejercicio2/2.py
+++ Ejercicio2/2.py
@@ -1,18 +1,20 @@
 ing = int(input("Indique ingresos: $"))
-costoCasa = 10000000
+costoCasa = int(input("Ingrese el valor de la casa: "))
 
 if 0 < ing < 80000:
     años = 7
     pie = round(0.3 * costoCasa)
     pagoMensual = round((costoCasa - pie) / 84)
+    print("Pie: $" + str(pie))
+    print("Pago mensual por",años,"años: $" + str(pagoMensual))
 elif 80000 <= ing:
     años = 10
     pie = round(0.15 * costoCasa)
     pagoMensual = round((costoCasa - pie) / 120)
+    print("Pie: $" + str(pie))
+    print("Pago mensual por",años,"años: $" + str(pagoMensual))
 else:
     print("Error")
     pie = 0
     pagoMensual = 0
     años = "error"
-print("Pie: $" + str(pie))
-print("Pago mensual por",años,"años: $" + str(pagoMensual))
